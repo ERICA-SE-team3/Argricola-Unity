cb007b3e5afb65a83c791f69b5070d446207caec
ernati <ernati@naver.com>
2023-05-09 22:54:15 +0900

    coordinate

---
+++ Assets/CameraManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager: MonoBehaviour
+{
+    Vector3 position_camera_mainboard = new Vector3(0, 0, -1);
+    Vector3 position_camera_player1 = new Vector3(0, 4000, -1);
+    Vector3 position_camera_player2 = new Vector3(4000, 0, -1);
+    Vector3 position_camera_player3 = new Vector3(0, -4000, -1);
+    Vector3 position_camera_player4 = new Vector3(-4000, 0, -1);
+    public GameObject MainCamera;
+
+
+    public void Show_Player1()
+    {
+        MainCamera.transform.position = position_camera_player1;
+        Debug.Log(MainCamera.transform.position);
+    }
+
+    public void Show_Player2()
+    {
+        MainCamera.transform.position = position_camera_player2;
+        Debug.Log(MainCamera.transform.position);
+    }
+
+    public void Show_Player3()
+    {
+        MainCamera.transform.position = position_camera_player3;
+        Debug.Log(MainCamera.transform.position);
+    }
+
+    public void Show_Player4()
+    {
+        MainCamera.transform.position = position_camera_player4;
+        Debug.Log(MainCamera.transform.position);
+    }
+
+    public void Show_Mainboard()
+    {
+        MainCamera.transform.position = position_camera_mainboard;
+        Debug.Log(MainCamera.transform.position);
+    }
+}
