a322e67381a2decb9782dd8a6a1026aa586d51cd
hajoon <gkwns0516@gmail.com>
2023-05-15 02:09:15 +0900

    feat : loading UI 구현 및 빵굽기행동 코드수정
    빵굽기 할 행동 클릭 시 효과 적용 + 하단 버튼으로 수량 조절가능

--- Assets/02. Scripts/ActionBakingBread.cs
+++ Assets/02. Scripts/ActionBakingBread.cs
@@ -5,45 +5,9 @@ using UnityEngine.UI;
 
 public class ActionBakingBread : MonoBehaviour
 {
-    public Image displayImage; // 클릭한 이미지
-    public Image breadImage; // 클릭하면 뜨게될 이미지
-
-    void Start()
+     public void MoveMe(GameObject dest)
     {
-        // displayImage 오브젝트 가져오기
-        displayImage = GetComponent<Image>();
-        breadImage = GetComponent<Image>();
-
-        // displayImage 오브젝트가 null인 경우 오류 처리
-        if (displayImage == null)
-        {
-            Debug.LogError("displayImage is null");
-            return;
-        }
-    }
-
-    void OnClick()
-    {
-        // breadImage가 null인 경우 오류 처리
-        if (breadImage == null)
-        {
-            Debug.LogError("breadImage is null");
-            return;
-        }
-
-        // displayImage가 null인 경우 오류 처리
-        if (displayImage == null)
-        {
-            Debug.LogError("displayImage is null");
-            return;
-        }
-
-        // displayImage에 breadImage 이미지 설정
-        breadImage.sprite = displayImage.sprite;
-    }
-
-        public void SelectBakingCard()
-    {
-        OnClick();
+        this.transform.SetParent(dest.transform);
+        this.transform.localPosition = Vector3.zero;
     }
 }
