2f70aea306adbf78f0d4a60bba8a7aa0099a1a70
Junhyeong_Choi <junhyung9571@naver.com>
2023-06-04 10:48:12 +0900

    Feat: FenceStrategy

--- Assets/02. Scripts/04. BoardEventStrategy/SowingEventStrategy.cs
+++ Assets/02. Scripts/04. BoardEventStrategy/SowingEventStrategy.cs
@@ -8,26 +8,13 @@ public class SowingEventStrategy : BoardEventStrategy
     public override void OnHoverEnter(Block block) 
     {
         PlayerBoard board = block.board;
-        if(isSowingAvailable(block)) 
-        {
-            Debug.Log("Sowing is available");
-            block.ShowGreen();
-        }
-        else if (!board.selectedBlocks.Contains(block))
-        {
-            Debug.Log("Sowing is unavailable");
-            block.ShowRed();
-        }
+
+        if( !isSowingAvailable(block) ) { block.ShowRed(); }
     }
 
     public override void OnHoverExit(Block block) 
     { 
-        PlayerBoard board = block.board;
-        
-        if(!board.selectedBlocks.Contains(block))
-        {
-            block.ShowTransparent();
-        }
+        block.ShowTransparent();
     }
 
     public override void OnClick(Block block) 
@@ -46,31 +33,8 @@ public class SowingEventStrategy : BoardEventStrategy
         }
     }
 
-    bool isSowingAvailable(Block block)
+    public bool isSowingAvailable(Block block)
     {
-        PlayerBoard board = block.board;
-
-        // 선택한 애들이랑, 기존 fence들까지 해서 인접해있는지 확인.
-
-        // Block should be empty
-        bool isBlockFarm;
-        // Block shouldn't be in selected blocks
-        bool isBlockInSelectedBlocks = board.selectedBlocks.Contains(block);
-
-        // Check if block is empty
-        isBlockEmpty = block.type == BlockType.EMPTY;
-        // Check if block is fence
-        isBlockFence = block.type == BlockType.FENCE;
-        // Check if block is adjacent to a fence
-        foreach(Block selectedBlock in board.selectedBlocks)
-        {
-            if(!block.IsAdjacentTo(selectedBlock))
-            {
-                isBlockAdjacentToFence = false;
-                break;
-            }
-        }
-
-        return isBlockEmpty && !isBlockFence && isBlockAdjacentToFence && !isBlockInSelectedBlocks;
+        return block.type == BlockType.FARM && block.seedType == SeedType.NONE;
     }
 }
