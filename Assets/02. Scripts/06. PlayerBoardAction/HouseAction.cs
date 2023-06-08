9d2066f3b197657940a9d257913d8b3fec61e429
김민석 <devwill409@gmail.com>
2023-06-08 16:00:01 +0900

    reafactoring: PlayerBoardAction, HouseAction

---
+++ Assets/02. Scripts/06. PlayerBoardAction/HouseAction.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
+
+public class HouseAction : PlayerBoardAction
+{
+    public override BoardEventStrategy StartInstall(GameObject confirmButton)
+    {
+        if (IsStartInstall())
+        {
+            BoardEventStrategy houseStrategy = new HouseEventStrategy();
+
+            Button button = confirmButton.GetComponent<Button>();
+            button.onClick.RemoveAllListeners();
+            button.onClick.AddListener(EndInstall);
+            return houseStrategy;
+        }
+        else
+        {
+            Debug.LogError("집 설치 행동을 시작할 수 없습니다.");
+        }
+    }
+
+    public override void EndInstall(List<Block> selectedBlocks)
+    {
+        if (IsEndInstall())
+        {
+            foreach (Block block in selectedBlocks)
+            {
+                block.ShowTransparent();
+                block.ChangeHouse();
+            }
+            selectedBlocks.Clear();
+        }
+        else
+        {
+            Debug.LogWarning("설치할 수 없습니다. 다시 선택해주세요.");
+        }
+    }
+
+    public override bool IsStartInstall()
+    {
+        Debug.LogError("설치 시작 전 가능한지 검사하는 함수 - 아직 구현 안됨");
+        return true;
+    }
+
+    public override bool IsEndInstall()
+    {
+        Debug.LogError("설치 가능한지 검사하는 함수 - 아직 구현 안됨");
+        return true;
+    }
+}
---
+++ Assets/02. Scripts/06. PlayerBoardAction/PlayerBoardAction.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
+
+public class PlayerBoardAction
+{
+    public virtual BoardEventStrategy StartInstall(GameObject confirmButton) { }
+
+    public virtual void EndInstall(List<Block> selectedBlocks) { }
+
+    public virtual bool IsStartInstall() { return true; }
+
+    public virtual bool IsEndInstall() { return true; }
+}
