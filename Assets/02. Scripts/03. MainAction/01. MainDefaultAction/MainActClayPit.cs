57cb94c05196d64fcb5dbfabb46da9f69044ef3b
manseong jeong <wjdakstjd@hanyang.ac.kr>
2023-06-10 00:22:44 +0900

    Update MainActClayPit.cs
    깨지는 주석 삭제

--- Assets/02. Scripts/03. MainAction/01. MainDefaultAction/MainActClayPit.cs
+++ Assets/02. Scripts/03. MainAction/01. MainDefaultAction/MainActClayPit.cs
@@ -4,27 +4,20 @@ using UnityEngine;
 
 public class MainActClayPit : ButtonParents
 {
-    //stack ���� ��������
     int stack;
 
     public override void OnClick()
     {
-        //stack ���� ��������
         stack = GameManager.instance.stackOfRoundCard[GameManager.instance.getStackBehavior("clayPit")];
 
-        // �ִٸ� �Ϲ� ��� �Լ� ȣ��
         ResourceManager.instance.addResource(GameManager.instance.getCurrentPlayerId(), "clay", stack * 2);
 
-        //Ȯ�� message
         Debug.Log("Player " + GameManager.instance.getCurrentPlayerId() + " get " + stack * 2 + " clay!");
 
-        //stack �ʱ�ȭ
         GameManager.instance.stackOfRoundCard[GameManager.instance.getStackBehavior("clayPit")] = 0;
 
-        //�ൿ�� �� �� ���� �� �ϳ� ���̱�
         ResourceManager.instance.minusResource(GameManager.instance.getCurrentPlayerId(), "family", 1);
 
-        //turn�� �����ٴ� flag 
         GameManager.instance.endTurnFlag = true;
 
     }
