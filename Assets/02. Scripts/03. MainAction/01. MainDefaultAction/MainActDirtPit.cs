6b607f5921e6fe2d089f769cc000ea47353fc7dd
LightYe4r <24adrenalin@naver.com>
2023-06-10 00:20:50 +0900

    Feat: mainAction + sidebar

--- Assets/02. Scripts/03. MainAction/01. MainDefaultAction/MainActDirtPit.cs
+++ Assets/02. Scripts/03. MainAction/01. MainDefaultAction/MainActDirtPit.cs
@@ -5,27 +5,21 @@ using UnityEngine;
 public class MainActDirtPit : ButtonParents
 {
 
-    //stack ���� ��������
     int stack;
 
     public override void OnClick()
     {
-        //stack ���� ��������
+
         stack = GameManager.instance.stackOfRoundCard[GameManager.instance.getStackBehavior("dirtPit")];
 
-        // �ִٸ� �Ϲ� ��� �Լ� ȣ��
         ResourceManager.instance.addResource(GameManager.instance.getCurrentPlayerId(), "clay", stack * 1);
 
-        //Ȯ�� message
         Debug.Log("Player " + GameManager.instance.getCurrentPlayerId() + " get " + stack * 2 + " clay!");
 
-        //stack �ʱ�ȭ
         GameManager.instance.stackOfRoundCard[GameManager.instance.getStackBehavior("dirtPit")] = 0;
 
-        //�ൿ�� �� �� ���� �� �ϳ� ���̱�
         ResourceManager.instance.minusResource(GameManager.instance.getCurrentPlayerId(), "family", 1);
 
-        //turn�� �����ٴ� flag 
         GameManager.instance.endTurnFlag = true;
     }
 }
