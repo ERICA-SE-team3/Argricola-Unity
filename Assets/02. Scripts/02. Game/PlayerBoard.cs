3ecbbeac46cc54e44a7d08e543b9f3357e694bbc
ernati <ernati@naver.com>
2023-06-11 01:51:32 +0900

    .

--- Assets/02. Scripts/02. Game/PlayerBoard.cs
+++ Assets/02. Scripts/02. Game/PlayerBoard.cs
@@ -215,6 +215,7 @@ public class PlayerBoard : MonoBehaviour
     /// </summary>
     bool isHouseInstallEndAvailable()
     {
+        
         Debug.LogWarning("설치 가능한지 검사하는 함수 - 아직 구현 안됨");
         return true;
     }
