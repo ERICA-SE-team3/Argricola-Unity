cc8c2db993ac02b7aea4f88b2ed4d55d1fb34115
Junhyung-Choi <junhyung9571@naver.com>
2023-06-11 15:57:09 +0900

    feat : scoreboard logic

--- Assets/02. Scripts/05. GameManager/ScoreBoard.cs
+++ Assets/02. Scripts/05. GameManager/ScoreBoard.cs
@@ -1,12 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class ScoreBoard : ButtonParents
+using UnityEngine.UI;
+public class ScoreBoard : MonoBehaviour
 {
-    public GameObject scoreBoard;
-
-    public override void OnClick(){
-        Instantiate(scoreBoard);
+    public List<GameObject> playerScoreBoards;
+    public void UpdateScore()
+    {
+        GameObject playerScoreBoard;
+        int[] scoreIndex = new int[15];
+        for(int playerIndex = 0; playerIndex < 4; playerIndex++){
+            playerScoreBoard = playerScoreBoards[playerIndex];
+            int sum = 0;
+            //scoreIndex[0] = GameManager.instance.players[playerIndex].filed.ToString();
+            //scoreIndex[0] = GameManager.instance.players[playerIndex].hutch.ToString();
+            scoreIndex[0] = GameManager.instance.players[playerIndex].wheat;
+            scoreIndex[0] = GameManager.instance.players[playerIndex].vegetable;
+            scoreIndex[0] = GameManager.instance.players[playerIndex].sheep;
+            scoreIndex[0] = GameManager.instance.players[playerIndex].pig;
+            scoreIndex[0] = GameManager.instance.players[playerIndex].cow;
+            //scoreIndex[0] = GameManager.instance.players[playerIndex].blank;
+            scoreIndex[0] = GameManager.instance.players[playerIndex].shed;
+            scoreIndex[0] = GameManager.instance.players[playerIndex].room;
+            //scoreIndex[0] = GameManager.instance.players[playerIndex].room;
+            scoreIndex[0] = GameManager.instance.players[playerIndex].family;
+            scoreIndex[0] = GameManager.instance.players[playerIndex].begging;
+            //scoreIndex[0] = GameManager.instance.players[playerIndex].card;
+            //scoreIndex[0] = GameManager.instance.players[playerIndex].additional;
+            //this.transform.Find("scoreboardField").Find("NumberBox").Find("NumberCounter").GetComponent<Text>().text = scoreIndex[0].ToString();
+            //this.transform.Find("scoreboardHutch").Find("NumberBox").Find("NumberCounter").GetComponent<Text>().text = scoreIndex[0].ToString();
+            playerScoreBoard.transform.Find("scoreboardWheat").Find("NumberBox1").Find("NumberCounter").GetComponent<Text>().text = scoreIndex[0].ToString();
+            playerScoreBoard.transform.Find("scoreboardVegetable").Find("NumberBox1").Find("NumberCounter").GetComponent<Text>().text = scoreIndex[0].ToString();
+            playerScoreBoard.transform.Find("scoreboardSheep").Find("NumberBox1").Find("NumberCounter").GetComponent<Text>().text = scoreIndex[0].ToString();
+            playerScoreBoard.transform.Find("scoreboardPig").Find("NumberBox1").Find("NumberCounter").GetComponent<Text>().text = scoreIndex[0].ToString();
+            playerScoreBoard.transform.Find("scoreboardCow").Find("NumberBox1").Find("NumberCounter").GetComponent<Text>().text = scoreIndex[0].ToString();
+            //playerScoreBoard.transform.Find("scoreboardBlank").Find("NumberBox").Find("NumberCounter").GetComponent<Text>().text = scoreIndex[0].ToString();
+            playerScoreBoard.transform.Find("scoreboardShed").Find("NumberBox1").Find("NumberCounter").GetComponent<Text>().text = scoreIndex[0].ToString();
+            playerScoreBoard.transform.Find("scoreboardClayhouse").Find("NumberBox1").Find("NumberCounter").GetComponent<Text>().text = scoreIndex[0].ToString();
+            //playerScoreBoard.transform.Find("scoreboardRockhouse").Find("NumberBox").Find("NumberCounter").GetComponent<Text>().text = scoreIndex[0].ToString();
+            playerScoreBoard.transform.Find("scoreboardFamily").Find("NumberBox1").Find("NumberCounter").GetComponent<Text>().text = scoreIndex[0].ToString();
+            playerScoreBoard.transform.Find("scoreboardBegging").Find("NumberBox1").Find("NumberCounter").GetComponent<Text>().text = scoreIndex[0].ToString();
+            //playerScoreBoard.transform.Find("scoreboardCard").Find("NumberBox").Find("NumberCounter").GetComponent<Text>().text = scoreIndex[0].ToString();
+            //playerScoreBoard.transform.Find("scoreboardAdditional").Find("NumberBox").Find("NumberCounter").GetComponent<Text>().text = scoreIndex[0].ToString();
+            
+        }
     }
 }
